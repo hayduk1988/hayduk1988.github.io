7c920ed452a57deea5890ca2cb7e21297253da8a
hayduk1988 <57803957+hayduk1988@users.noreply.github.com>
2024-07-31 13:18:19 +0400

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8395be2b261ada28a63fe8d31f8a6ecc",
+const RESOURCES = {"flutter_bootstrap.js": "b134918074216f9464263bbd65f927c3",
 "version.json": "ea0072147c4b523ef3bce733e9b609a1",
 "index.html": "8ea7a30b6a5fb79257fb977605228afa",
 "/": "8ea7a30b6a5fb79257fb977605228afa",
-"main.dart.js": "a87dccc1c2f4e8be6829ac2c2308b7d9",
+"main.dart.js": "b6dbf729c0f440e17264b53c418262ad",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
