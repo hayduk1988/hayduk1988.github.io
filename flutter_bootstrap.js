1b7f009c34dbc9e31245989eb2876d62bff93ce9
hayduk1988 <57803957+hayduk1988@users.noreply.github.com>
2024-08-06 21:22:16 +0400

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "290326772"
+    serviceWorkerVersion: "3309120082"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3eec5a46e2fbeb9c2149c03a21a6cd1d",
+const RESOURCES = {"flutter_bootstrap.js": "94d9971f2b86184eac08a0488a1405fb",
 "version.json": "ea0072147c4b523ef3bce733e9b609a1",
 "index.html": "8ea7a30b6a5fb79257fb977605228afa",
 "/": "8ea7a30b6a5fb79257fb977605228afa",
-"main.dart.js": "0b407be71da29e405a82a642d918038a",
+"main.dart.js": "448fb1a7f126bb0536d1856af385ba94",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
